55c431cc8d5890744148db5af39aae156a163eb7
Journii <ginopaflo001608@gmail.com>
2023-02-27 11:34:19 -0500

    Adicion de vista para los administradores

--- controller/MainpageController.php
+++ controller/MainpageController.php
@@ -5,9 +5,21 @@ class MainpageController extends AbstractController {
     
     public function show()
     {  
+<<<<<<< Updated upstream:controller/MainpageController.php
         $this->renderView('header');
         $this->renderView('main');
         $this->renderView('footer');
+=======
+        session_start();
+        if(isset($_SESSION['username']) && isset($_SESSION['tipoUser']) ){
+            $this->renderView(_ROOT_VIEWS . 'header');
+            //agregar demas comandos
+            $this->renderView(_ROOT_VIEWS . 'footer');
+        }else{
+            header('Location: /administrador');
+            exit;
+        }
+>>>>>>> Stashed changes:controller/AdminMainpage.php
         
     }
 
