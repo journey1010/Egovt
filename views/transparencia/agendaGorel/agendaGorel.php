f599d316b80e0477d58124de1afa986760f502f8
JHEROPIAM <jpindoamias@gmail.com>
2023-05-11 13:19:59 -0500

    agenda gobernador

---
+++ views/transparencia/agendaGorel/agendaGorel.php
@@ -0,0 +1,46 @@
+<main>
+    <section class="ItemfullBlock p-3">
+        <div class="container p-3">
+            <h3 class="text-blue mb-2 text-center">AGENDA DEL GOBIERNO REGIONAL DE LORETA</h3>
+            <strong class="d-block text-secondary mb-3 text-center">SEDE CENTRAL</strong>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <article class="npbColumn shadow bg-white mb-6 mb-xl-12">
+                        <div class="imgHolder position-relative">
+                            <time datetime="2023-05-08" class="npbTimeTag font-weight-bold fontAlter position-absolute text-white px-2 py-1">08 May 2023</time>
+                        </div>
+                        <div class="npbDescriptionWrap px-5 pt-8 pb-5">
+                            <strong class="d-block npbcmWrap font-weight-normal mb-1">
+                                <i class="icnTheme fwMedium icomoon-clock"><span class="sr-only">icon</span></i>
+                                <strong class="text-lDark title">Hora: </strong><time datetime="07:00">07:00</time>
+                                <i class="fas fa-clipboard-user"><span class="sr-only">icon</span></i>
+                            </strong>
+                            <h3 class="fwSemiBold mb-1">
+                            <strong class="text-lDark title">Problemáticas Que Aquejan A Los Institutos De Educación Superior</strong>
+                            </h3>
+                            <p>Reunión con el Lic. Edu. Adan Martin Corena quintanilla</p>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <nav aria-label="Page navigation">
+                <ul class="pagination justify-content-center pt-2">
+                    <li class="page-item disabled">
+                        <a class="page-link" href="javascript:void(0);">Previous</a>
+                    </li>
+                    <li class="page-item active" aria-current="page">
+                        <a class="page-link" href="javascript:void(0);">1 <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="javascript:void(0);">2</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="javascript:void(0);">Next <i class="fas fa-chevron-right icn"><span class="sr-only">icon</span></i></a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </section>
+</main>
