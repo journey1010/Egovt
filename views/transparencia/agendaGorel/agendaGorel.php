01f5bb8677633764a6aeb25791676f34f56a583f
JHEROPIAM <jpindoamias@gmail.com>
2023-05-15 10:54:02 -0500

    agenda gobernador incompleto

--- views/transparencia/agendaGorel/agendaGorel.php
+++ views/transparencia/agendaGorel/agendaGorel.php
@@ -1,3 +1,4 @@
+
 <main>
     <section class="ItemfullBlock p-3">
         <div class="container p-3">
@@ -11,18 +12,195 @@
                         <div class="imgHolder position-relative">
                             <time datetime="2023-05-08" class="npbTimeTag font-weight-bold fontAlter position-absolute text-white px-2 py-1">08 May 2023</time>
                         </div>
-                        <div class="npbDescriptionWrap px-5 pt-8 pb-5">
-                            <strong class="d-block npbcmWrap font-weight-normal mb-1">
-                                <i class="icnTheme fwMedium icomoon-clock"><span class="sr-only">icon</span></i>
-                                <strong class="text-lDark title">Hora: </strong><time datetime="07:00">07:00</time>
-                                <i class="fas fa-clipboard-user"><span class="sr-only">icon</span></i>
+                        <div class="npbDescriptionWrap px-5 pt-8 pb-3">
+                            <strong class="text-lDark mb-1">
+                                <h3 class="fwSemiBold mb-1">
+                                    <strong class="text-lDark title">Problemáticas Que Aquejan A Los Institutos De Educación Superior</strong>
+                                </h3>
+                                <p class="text-muted">Reunión con el Lic. Edu. Adan Martin Corena quintanilla</p>
+                                <ul class="list-unstyled contactInfoList mb-3">
+                                    <li>
+                                        <i class="text-lDark icnTheme fwMedium icomoon-clock icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Hora: </strong>
+                                        <strong class="text-muted"><time datetime="07:00">07:00</time></strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-clipboard-user icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Organiza: </strong>
+                                        <strong class="text-muted">Protocolo</strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-map-marker-alt icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Lugar: </strong>
+                                        <strong class="text-muted">Despacho de Gobernación</strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-users icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Participantes: </strong>
+                                        <strong class="text-muted">
+                                            Dr. Jorge René Chávez Silvano- Gobernador Regional de Loreto, Gerencia de infraestructura, Gerencia de educación
+                                        </strong>
+                                    </li>
+                                </ul>
                             </strong>
-                            <h3 class="fwSemiBold mb-1">
-                            <strong class="text-lDark title">Problemáticas Que Aquejan A Los Institutos De Educación Superior</strong>
-                            </h3>
-                            <p>Reunión con el Lic. Edu. Adan Martin Corena quintanilla</p>
                         </div>
-                    </article>
+                    </article>                    
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <article class="npbColumn shadow bg-white mb-6 mb-xl-12">
+                        <div class="imgHolder position-relative">
+                            <time datetime="2023-05-08" class="npbTimeTag font-weight-bold fontAlter position-absolute text-white px-2 py-1">08 May 2023</time>
+                        </div>
+                        <div class="npbDescriptionWrap px-5 pt-8 pb-3">
+                            <strong class="text-lDark mb-1">
+                                <h3 class="fwSemiBold mb-1">
+                                    <strong class="text-lDark title">Problemáticas Que Aquejan A Los Institutos De Educación Superior</strong>
+                                </h3>
+                                <p class="text-muted">Reunión con el Lic. Edu. Adan Martin Corena quintanilla</p>
+                                <ul class="list-unstyled contactInfoList mb-3">
+                                    <li>
+                                        <i class="text-lDark icnTheme fwMedium icomoon-clock icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Hora: </strong>
+                                        <strong class="text-muted"><time datetime="07:00">07:00</time></strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-clipboard-user icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Organiza: </strong>
+                                        <strong class="text-muted">Protocolo</strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-map-marker-alt icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Lugar: </strong>
+                                        <strong class="text-muted">Despacho de Gobernación</strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-users icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Participantes: </strong>
+                                        <strong class="text-muted">
+                                            Dr. Jorge René Chávez Silvano- Gobernador Regional de Loreto, 
+                                        </strong>
+                                    </li>
+                                </ul>
+                            </strong>
+                        </div>
+                    </article>                    
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <article class="npbColumn shadow bg-white mb-6 mb-xl-12">
+                        <div class="imgHolder position-relative">
+                            <time datetime="2023-05-08" class="npbTimeTag font-weight-bold fontAlter position-absolute text-white px-2 py-1">08 May 2023</time>
+                        </div>
+                        <div class="npbDescriptionWrap px-5 pt-8 pb-3">
+                            <strong class="text-lDark mb-1">
+                                <h3 class="fwSemiBold mb-1">
+                                    <strong class="text-lDark title">Problemáticas Que Aquejan A Los Institutos De Educación Superior</strong>
+                                </h3>
+                                <p class="text-muted">Reunión con el Lic. Edu. Adan Martin Corena quintanilla</p>
+                                <ul class="list-unstyled contactInfoList mb-3">
+                                    <li>
+                                        <i class="text-lDark icnTheme fwMedium icomoon-clock icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Hora: </strong>
+                                        <strong class="text-muted"><time datetime="07:00">07:00</time></strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-clipboard-user icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Organiza: </strong>
+                                        <strong class="text-muted">Protocolo</strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-map-marker-alt icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Lugar: </strong>
+                                        <strong class="text-muted">Despacho de Gobernación</strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-users icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Participantes: </strong>
+                                        <strong class="text-muted">
+                                            Dr. Jorge René Chávez Silvano- Gobernador Regional de Loreto, Gerencia de infraestructura, Gerencia de educación
+                                        </strong>
+                                    </li>
+                                </ul>
+                            </strong>
+                        </div>
+                    </article>                    
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <article class="npbColumn shadow bg-white mb-6 mb-xl-12">
+                        <div class="imgHolder position-relative">
+                            <time datetime="2023-05-08" class="npbTimeTag font-weight-bold fontAlter position-absolute text-white px-2 py-1">08 May 2023</time>
+                        </div>
+                        <div class="npbDescriptionWrap px-5 pt-8 pb-3">
+                            <strong class="text-lDark mb-1">
+                                <h3 class="fwSemiBold mb-1">
+                                    <strong class="text-lDark title">Problemáticas Que Aquejan A Los Institutos De Educación Superior</strong>
+                                </h3>
+                                <p class="text-muted">Reunión con el Lic. Edu. Adan Martin Corena quintanilla</p>
+                                <ul class="list-unstyled contactInfoList mb-3">
+                                    <li>
+                                        <i class="text-lDark icnTheme fwMedium icomoon-clock icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Hora: </strong>
+                                        <strong class="text-muted"><time datetime="07:00">07:00</time></strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-clipboard-user icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Organiza: </strong>
+                                        <strong class="text-muted">Protocolo</strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-map-marker-alt icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Lugar: </strong>
+                                        <strong class="text-muted">Despacho de Gobernación</strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-users icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Participantes: </strong>
+                                        <strong class="text-muted">
+                                            Dr. Jorge René Chávez Silvano- Gobernador Regional de Loreto, Gerencia de infraestructura, Gerencia de educación
+                                        </strong>
+                                    </li>
+                                </ul>
+                            </strong>
+                        </div>
+                    </article>                    
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <article class="npbColumn shadow bg-white mb-6 mb-xl-12">
+                        <div class="imgHolder position-relative">
+                            <time datetime="2023-05-08" class="npbTimeTag font-weight-bold fontAlter position-absolute text-white px-2 py-1">08 May 2023</time>
+                        </div>
+                        <div class="npbDescriptionWrap px-5 pt-8 pb-3">
+                            <strong class="text-lDark mb-1">
+                                <h3 class="fwSemiBold mb-1">
+                                    <strong class="text-lDark title">Problemáticas Que Aquejan A Los Institutos De Educación Superior</strong>
+                                </h3>
+                                <p class="text-muted">Reunión con el Lic. Edu. Adan Martin Corena quintanilla</p>
+                                <ul class="list-unstyled contactInfoList mb-3">
+                                    <li>
+                                        <i class="text-lDark icnTheme fwMedium icomoon-clock icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Hora: </strong>
+                                        <strong class="text-muted"><time datetime="07:00">07:00</time></strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-clipboard-user icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Organiza: </strong>
+                                        <strong class="text-muted">Protocolo</strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-map-marker-alt icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Lugar: </strong>
+                                        <strong class="text-muted">Despacho de Gobernación</strong>
+                                    </li>
+                                    <li>
+                                        <i class="text-lDark fas fa-users icn position-absolute"><span class="sr-only">icon</span></i>
+                                        <strong class="text-lDark title">Participantes: </strong>
+                                        <strong class="text-muted">
+                                            Dr. Jorge René Chávez Silvano- Gobernador Regional de Loreto, Gerencia de infraestructura, Gerencia de educación
+                                        </strong>
+                                    </li>
+                                </ul>
+                            </strong>
+                        </div>
+                    </article>                    
                 </div>
             </div>
             <nav aria-label="Page navigation">
