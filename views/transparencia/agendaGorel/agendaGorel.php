32a27acf555d821487d7a742ab657ca0b21bdcf6
Journii <ginopaflo001608@gmail.com>
2023-05-23 13:32:03 -0500

    Seccion de regularizar visitas terminado

--- views/transparencia/agendaGorel/agendaGorel.php
+++ views/transparencia/agendaGorel/agendaGorel.php
@@ -2,7 +2,7 @@
 <main>
     <section class="ItemfullBlock p-3">
         <div class="container p-3">
-            <h3 class="text-blue mb-2 text-center">AGENDA DEL GOBIERNO REGIONAL DE LORETA</h3>
+            <h3 class="text-blue mb-2 text-center">AGENDA DEL GOBERNADOR REGIONAL DE LORETO</h3>
             <strong class="d-block text-secondary mb-3 text-center">SEDE CENTRAL</strong>
         </div>
         <div class="container">
