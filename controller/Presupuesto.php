50deb297543564a9ddaadda3ceb62ae2d0ec2879
Journii <ginopaflo001608@gmail.com>
2023-09-22 14:57:51 -0500

    saldo balance: model

---
+++ controller/Presupuesto.php
@@ -0,0 +1,12 @@
+<?php
+require_once _ROOT_MODEL . 'PresupuestoModel.php';
+require_once _ROOT_CONTROLLER . 'BaseViewInterfaz.php';
+
+class Presupuesto extends BaseViewInterfaz
+{
+
+    public function showSaldo()
+    {
+        
+    }
+}
---
+++ model/PresupuestoModel.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once _ROOT_MODEL . 'conexion.php';
+require_once(_ROOT_CONTROLLER . 'paginador.php');
+
+class PresupuestoModel 
+{
+    public static function getSaldoBalance($pagina)
+    {   
+        session_set_cookie_params(['lifetime' => 60]);
+        session_start();
+
+        $resultsPerPage = 10;
+        $sql = 'SELECT title, CONCAT(YEAR(load_date), '/',MONTH(load_date), '/', path_file ), load_date  FROM saldos_de_balance';
+        $params = [];
+
+        if (isset($_SESSION['saldo_balance'])) {
+            $paginador = $_SESSION['saldo_balance'];
+            $paginadorHTML = $paginador->setResultadosPorPagina($resultsPerPage);
+        } else {
+            $conexion = new MySQLConnection();
+            $paginador = new Paginator($conexion, $sql, $params, $pagina, $resultsPerPage);
+            $_SESSION['saldo_balance'] = $paginador;
+        }
+
+        $paginador->setPaginaActual($pagina);
+        $resultados = $paginador->getResultados();
+        $paginadorHTML = $paginador->obtenerPaginador();
+        return [$resultados, $paginadorHTML];
+    }
+}
